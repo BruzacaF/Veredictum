8e24c45893ee21c720b743a8cf0a8979cb748397
Bruza <fbruzacac@gmail.com>
2025-11-10 20:02:05 -0300

    chore: estrutura inicial do projeto
    - Adiciona estrutura completa de diretórios e pacotes
    - Inclui arquivos de configuração e documentação base
    - Define arquitetura por camadas (Controller, Service, Repository, Model, DTOs, Views)
    - Sem implementação de código ainda

---
+++ src/main/java/br/edu/ifpb/pweb2/veredictum/dto/TestDto.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.pweb2.veredictum.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class TestDto {
+
+}
---
+++ src/main/java/br/edu/ifpb/pweb2/veredictum/enums/TestEnum.java
@@ -0,0 +1,4 @@
+package br.edu.ifpb.pweb2.veredictum.enums;
+
+public class TestEnum {
+}
