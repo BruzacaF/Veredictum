7d57b3d3228a805a49d1182eaa6c30d4b208fac0
Bruza <fbruzacac@gmail.com>
2025-11-18 17:57:54 -0300

    feat: adicionado sistema de auth

--- src/main/java/br/edu/ifpb/pweb2/veredictum/controller/HomeController.java
+++ src/main/java/br/edu/ifpb/pweb2/veredictum/controller/HomeController.java
@@ -1,8 +1,6 @@
 package br.edu.ifpb.pweb2.veredictum.controller;
 
 import br.edu.ifpb.pweb2.veredictum.security.UsuarioDetails;
-import br.edu.ifpb.pweb2.veredictum.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 
 public class HomeController {
-    @Autowired
-    private UsuarioService usuarioService;
 
     @GetMapping("/home")
     public String home(Model model, @AuthenticationPrincipal UsuarioDetails usuarioDetails) {
         model.addAttribute("usuario", usuarioDetails.getUsuario());
         return "home";
     }
+
+
 
 
 
--- src/main/java/br/edu/ifpb/pweb2/veredictum/service/UsuarioService.java
+++ src/main/java/br/edu/ifpb/pweb2/veredictum/service/UsuarioService.java
@@ -1,22 +1,8 @@
 package br.edu.ifpb.pweb2.veredictum.service;
 
-import br.edu.ifpb.pweb2.veredictum.model.Usuario;
-import br.edu.ifpb.pweb2.veredictum.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-    @Autowired
-    private UsuarioDetailsService usuarioDetailsService;
-
-    public Usuario getUsuarioLogado() {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return usuarioRepository.findByEmail(email);
-
-    }
 }
