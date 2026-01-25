10f2bdb1c768ec3250eae71c4ae51004471558c5
Bruza <fbruzacac@gmail.com>
2026-01-25 13:52:34 -0300

    feat:implementado listar Processos Professor

--- src/main/java/br/edu/ifpb/pweb2/veredictum/controller/HomeController.java
+++ src/main/java/br/edu/ifpb/pweb2/veredictum/controller/HomeController.java
@@ -1,12 +1,19 @@
 package br.edu.ifpb.pweb2.veredictum.controller;
 
+import br.edu.ifpb.pweb2.veredictum.model.Aluno;
+import br.edu.ifpb.pweb2.veredictum.model.Processo;
+import br.edu.ifpb.pweb2.veredictum.model.Professor;
 import br.edu.ifpb.pweb2.veredictum.repository.AssuntoRepository;
+import br.edu.ifpb.pweb2.veredictum.security.UsuarioDetails;
 import br.edu.ifpb.pweb2.veredictum.service.ProcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 
@@ -18,8 +25,22 @@ public class HomeController {
     private AssuntoRepository assuntoRepository;
 
     @GetMapping("/aluno")
-    public String home(Model model) {
+    public String home(Model model, @AuthenticationPrincipal UsuarioDetails usuario) {
+
+        List<Processo> processos = processoService.buscarPorAluno((Aluno) usuario.getUsuario());
+        model.addAttribute("processos", processos);
 
         return "aluno/dashboard";
     }
+
+    @GetMapping("/professor")
+    public String professor(Model model, @AuthenticationPrincipal UsuarioDetails usuario) {
+
+        List<Processo> processos = processoService.buscarPorProfessorRelator((Professor) usuario.getUsuario());
+        model.addAttribute("processos", processos);
+        return "professor/dashboard";
+    }
+
+
+
 }
--- src/main/java/br/edu/ifpb/pweb2/veredictum/controller/controllerAdvice/DashboardControllerAdvice.java
+++ src/main/java/br/edu/ifpb/pweb2/veredictum/controller/controllerAdvice/DashboardControllerAdvice.java
@@ -1,51 +1,64 @@
 package br.edu.ifpb.pweb2.veredictum.controller.controllerAdvice;
 
 
-import br.edu.ifpb.pweb2.veredictum.dto.ProcessoDTO;
 import br.edu.ifpb.pweb2.veredictum.enums.RoleEnum;
 import br.edu.ifpb.pweb2.veredictum.enums.StatusProcessoEnum;
-import br.edu.ifpb.pweb2.veredictum.model.Aluno;
+import br.edu.ifpb.pweb2.veredictum.model.Assunto;
+import br.edu.ifpb.pweb2.veredictum.model.Processo;
 import br.edu.ifpb.pweb2.veredictum.model.Usuario;
 import br.edu.ifpb.pweb2.veredictum.repository.AssuntoRepository;
 import br.edu.ifpb.pweb2.veredictum.security.UsuarioDetails;
-import br.edu.ifpb.pweb2.veredictum.service.ProcessoService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.Collections;
+import java.util.List;
+
 @ControllerAdvice
 public class DashboardControllerAdvice {
-    @Autowired
-    private ProcessoService processoService;
-    @Autowired
-    private AssuntoRepository assuntoRepository;
 
-    @ModelAttribute
-    public void addDashboardAttributes(HttpServletRequest request,
-                                       Model model,
-                                       @AuthenticationPrincipal UsuarioDetails usuario) {
-        String uri = request.getRequestURI();
+    private final AssuntoRepository assuntoRepository;
 
-        if (uri.startsWith("/home/aluno")) {
-            if(!model.containsAttribute("processoDTO")){
-                model.addAttribute("processoDTO", new ProcessoDTO());
-            }
+    public DashboardControllerAdvice(
+                                     AssuntoRepository assuntoRepository) {
 
-            if (usuario != null) {
-                Usuario user = usuario.getUsuario();
-                model.addAttribute("usuario", user);
-
-                if (user.getRole() == RoleEnum.ALUNO) {
-                    model.addAttribute("processos", processoService.buscarPorAluno((Aluno) user));
-                }
-            }
-
-            model.addAttribute("status", StatusProcessoEnum.values());
-            model.addAttribute("assuntos", assuntoRepository.findAll());
-        }
+        this.assuntoRepository = assuntoRepository;
     }
 
+    @ModelAttribute("usuario")
+    public Usuario usuarioLogado(@AuthenticationPrincipal UsuarioDetails principal) {
+        return principal != null ? principal.getUsuario() : null;
+    }
+
+    @ModelAttribute("statusProcesso")
+    public StatusProcessoEnum[] statusProcesso() {
+        return StatusProcessoEnum.values();
+    }
+
+    @ModelAttribute("assuntos")
+    public List<Assunto> assuntos() {
+        return assuntoRepository.findAll();
+    }
+
+    @ModelAttribute("processos")
+    public List<Processo> processosPadrao() {
+        return Collections.emptyList();
+    }
+
+    @ModelAttribute("isProfessor")
+    public boolean isProfessor(@AuthenticationPrincipal UsuarioDetails usuario) {
+        if (usuario != null) {
+            return usuario.getUsuario().getRole() == RoleEnum.PROFESSOR;
+        }
+        return false;
+    }
+
+    @ModelAttribute("isAluno")
+    public boolean isAluno(@AuthenticationPrincipal UsuarioDetails usuario) {
+        if (usuario != null) {
+            return usuario.getUsuario().getRole() == RoleEnum.ALUNO;
+        }
+        return false;
+    }
 }
